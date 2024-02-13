66c60fdb70a439cf19e9d466d6d2d273b89ca724
agnieszkacieciwa <agnieszka.cieciwa@gmail.com>
2024-02-13 15:40:46 +0000

    Update2

--- user_input_data.py
+++ user_input_data.py
@@ -58,5 +58,25 @@ def search_results(author, title):
     else:
         return render_template('search_results.html', itunes_data=None)
 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+
+        if not author or not title:
+            error_message = 'Wprowadź zarówno autora, jak i tytuł ebooka.'
+            return render_template('index.html', error_message=error_message)
+
+        # Przesyłamy dane do szablonu HTML w przypadku, gdy użytkownik dokonał wyszukiwania
+        itunes_data = search_itunes_api(author, title)
+        exchange_rate = get_nbp_exchange_rate('USD')  
+        
+        return render_template('search_results.html', itunes_data=itunes_data, exchange_rate=exchange_rate)
+
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
