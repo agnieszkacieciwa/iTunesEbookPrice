7371a0851fa4dbd4c8c95a6e54dba7c08cc356c0
agnieszkacieciwa <agnieszka.cieciwa@gmail.com>
2024-02-13 16:31:43 +0000

    Update4

--- user_input_data.py
+++ user_input_data.py
@@ -17,10 +17,6 @@ def index():
         # Przesyłamy dane do szablonu HTML w przypadku, gdy użytkownik dokonał wyszukiwania
         itunes_data = search_itunes_api(author, title)
         exchange_rate = get_nbp_exchange_rate('USD')  
-        
-        return render_template('search_results.html', itunes_data=itunes_data, exchange_rate=exchange_rate)
-
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
